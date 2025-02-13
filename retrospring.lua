0fbb73fd38c4b60b465e621b6a406ef7098ab12a
arkiver <arkiver@protonmail.com>
2025-02-13 16:17:27 +0100

    Version 20250213.01. Initial.

---
+++ retrospring.lua
@@ -0,0 +1,687 @@
+local urlparse = require("socket.url")
+local http = require("socket.http")
+local https = require("ssl.https")
+local cjson = require("cjson")
+local utf8 = require("utf8")
+local html_entities = require("htmlEntities")
+
+local item_dir = os.getenv("item_dir")
+local warc_file_base = os.getenv("warc_file_base")
+local concurrency = tonumber(os.getenv("concurrency"))
+local item_type = nil
+local item_name = nil
+local item_value = nil
+local item_user = nil
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local seen_200 = {}
+local addedtolist = {}
+local abortgrab = false
+local killgrab = false
+local logged_response = false
+
+local discovered_outlinks = {}
+local discovered_items = {}
+local bad_items = {}
+local ids = {}
+
+local retry_url = false
+local is_initial_url = true
+
+abort_item = function(item)
+  abortgrab = true
+  --killgrab = true
+  if not item then
+    item = item_name
+  end
+  if not bad_items[item] then
+    io.stdout:write("Aborting item " .. item .. ".\n")
+    io.stdout:flush()
+    bad_items[item] = true
+  end
+end
+
+kill_grab = function(item)
+  io.stdout:write("Aborting crawling.\n")
+  killgrab = true
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+processed = function(url)
+  if downloaded[url] or addedtolist[url] then
+    return true
+  end
+  return false
+end
+
+discover_item = function(target, item)
+  if not target[item] then
+--print("discovered", item)
+    target[item] = true
+    return true
+  end
+  return false
+end
+
+find_item = function(url)
+  if ids[url] then
+    return nil
+  end
+  local value = nil
+  local type_ = nil
+  for pattern, name in pairs({
+    ["^https://[^/]*retrospring%.net/@([0-9a-zA-Z_]+)$"]="account",
+    ["^https://[^/]*retrospring%.net/@([0-9a-zA-Z_]+/a/[0-9]+)$"]="answer",
+    ["^https://[^/]*retrospring%.net/@([0-9a-zA-Z_]+/q/[0-9]+)$"]="question",
+    ["^https?://([^/]*s3%.amazonaws%.com/.+)$"]="asset"
+  }) do
+    value = string.match(url, pattern)
+    type_ = name
+    if value then
+      break
+    end
+  end
+  if value and type_ then
+    return {
+      ["value"]=value,
+      ["type"]=type_
+    }
+  end
+end
+
+set_item = function(url)
+  found = find_item(url)
+  if found then
+    local newcontext = {}
+    new_item_type = found["type"]
+    new_item_value = found["value"]
+    if new_item_type ~= "asset" then
+      new_item_value = string.gsub(new_item_value, "/[aq]/", ":")
+    end
+    new_item_name = new_item_type .. ":" .. new_item_value
+    local post_id = string.match(new_item_value, ":([^:]+)$")
+    if new_item_name ~= item_name
+      and not ids[post_id] then
+      ids = {}
+      context = newcontext
+      item_value = new_item_value
+      item_type = new_item_type
+      ids[string.lower(item_value)] = true
+      if post_id then
+        ids[string.lower(post_id)] = true
+      end
+      abortgrab = false
+      tries = 0
+      retry_url = false
+      is_initial_url = true
+      item_name = new_item_name
+      print("Archiving item " .. item_name)
+    end
+  end
+end
+
+percent_encode_url = function(url)
+  temp = ""
+  for c in string.gmatch(url, "(.)") do
+    local b = string.byte(c)
+    if b < 32 or b > 126 then
+      c = string.format("%%%02X", b)
+    end
+    temp = temp .. c
+  end
+  return temp
+end
+
+allowed = function(url, parenturl)
+  local noscheme = string.match(url, "^https?://(.*)$")
+
+  if ids[url]
+    or (noscheme and ids[string.lower(noscheme)]) then
+    return true
+  end
+
+  if string.match(url, "^https?://[^/]+/[^/]+/c/[0-9]+/reactions$")
+    and (
+      not parenturl
+      or (
+        parenturl
+        and string.match(parenturl, "/comments$")
+      )
+    ) then
+    return true
+  end
+
+  if string.match(url, "^https?://twitter%.com/intent/tweet%?")
+    or string.match(url, "^https?://bsky%.app/intent/compose")
+    or string.match(url, "^https?://www%.tumblr%.com/widgets/share/tool%?")
+    or string.match(url, "^https?://t%.me/share/url%?") then
+    return false
+  end
+
+  local skip = false
+  for pattern, type_ in pairs({
+    ["^https://[^/]*retrospring%.net/@?([0-9a-zA-Z_]+)$"]="account",
+    ["^https://[^/]*retrospring%.net/@?([0-9a-zA-Z_]+/a/[0-9]+)"]="answer",
+    ["^https://[^/]*retrospring%.net/@?([0-9a-zA-Z_]+/q/[0-9]+)"]="question",
+    ["^https?://([^/]*s3%.amazonaws%.com/.+)$"]="asset"
+  }) do
+    match = string.match(url, pattern)
+    if match then
+      if type_ ~= "asset" then
+        match = string.gsub(match, "/[aq]/", ":")
+      end
+      local new_item = type_ .. ":" .. match
+      if new_item ~= item_name
+        and not ids[string.lower(string.match(match, "([^:]+)$"))] then
+        discover_item(discovered_items, new_item)
+        skip = true
+      end
+    end
+  end
+  if skip then
+    return false
+  end
+
+  if not string.match(url, "^https?://[^/]*retrospring%.net/") then
+    discover_item(discovered_outlinks, string.match(percent_encode_url(url), "^([^%s]+)"))
+    return false
+  end
+
+  for _, pattern in pairs({
+    "([0-9]+)",
+    "([0-9a-zA-Z_]+)"
+  }) do
+    for s in string.gmatch(url, pattern) do
+      if ids[string.lower(s)] then
+        return true
+      end
+    end
+  end
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+
+  --[[if allowed(url, parent["url"])
+    and not processed(url)
+    and string.match(url, "^https://")
+    and not addedtolist[url] then
+    addedtolist[url] = true
+    return true
+  end]]
+
+  return false
+end
+
+decode_codepoint = function(newurl)
+  newurl = string.gsub(
+    newurl, "\\[uU]([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])",
+    function (s)
+      return utf8.char(tonumber(s, 16))
+    end
+  )
+  return newurl
+end
+
+percent_encode_url = function(newurl)
+  result = string.gsub(
+    newurl, "(.)",
+    function (s)
+      local b = string.byte(s)
+      if b < 32 or b > 126 then
+        return string.format("%%%02X", b)
+      end
+      return s
+    end
+  )
+  return result
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+  local json = nil
+
+  local turbo_frame = nil
+
+  downloaded[url] = true
+
+  if abortgrab then
+    return {}
+  end
+
+  local function fix_case(newurl)
+    if not newurl then
+      newurl = ""
+    end
+    if not string.match(newurl, "^https?://[^/]") then
+      return newurl
+    end
+    if string.match(newurl, "^https?://[^/]+$") then
+      newurl = newurl .. "/"
+    end
+    local a, b = string.match(newurl, "^(https?://[^/]+/)(.*)$")
+    return string.lower(a) .. b
+  end
+
+  local function check(newurl)
+    if not string.match(newurl, "^https?://") then
+      return nil
+    end
+    local post_body = nil
+    local post_url = nil
+    if not newurl then
+      newurl = ""
+    end
+    newurl = decode_codepoint(newurl)
+    newurl = fix_case(newurl)
+    local origurl = url
+    if string.len(url) == 0
+      or string.len(newurl) == 0 then
+      return nil
+    end
+    local url = string.match(newurl, "^([^#]+)")
+    local url_ = string.match(url, "^(.-)[%.\\]*$")
+    while string.find(url_, "&amp;") do
+      url_ = string.gsub(url_, "&amp;", "&")
+    end
+    if not processed(url_)
+      and not processed(url_ .. "/")
+      and allowed(url_, origurl) then
+print('QUEUING', url_)
+      local headers = {}
+      if turbo_frame then
+        headers["turbo-frame"] = turbo_frame
+        turbo_frame = nil
+      end
+      table.insert(urls, {
+        url=url_,
+        headers=headers
+      })
+      addedtolist[url_] = true
+      addedtolist[url] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    if not newurl then
+      newurl = ""
+    end
+    newurl = decode_codepoint(newurl)
+    if string.match(newurl, "['\"><]") then
+      return nil
+    end
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^/") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^%.%./") then
+      if string.match(url, "^https?://[^/]+/[^/]+/") then
+        check(urlparse.absolute(url, newurl))
+      else
+        checknewurl(string.match(newurl, "^%.%.(/.+)$"))
+      end
+    elseif string.match(newurl, "^%./") then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    if not newurl then
+      newurl = ""
+    end
+    newurl = decode_codepoint(newurl)
+    if string.match(newurl, "^%?") then
+      check(urlparse.absolute(url, newurl))
+    elseif not (
+      string.match(newurl, "^https?:\\?/\\?//?/?")
+      or string.match(newurl, "^[/\\]")
+      or string.match(newurl, "^%./")
+      or string.match(newurl, "^[jJ]ava[sS]cript:")
+      or string.match(newurl, "^[mM]ail[tT]o:")
+      or string.match(newurl, "^vine:")
+      or string.match(newurl, "^android%-app:")
+      or string.match(newurl, "^ios%-app:")
+      or string.match(newurl, "^data:")
+      or string.match(newurl, "^irc:")
+      or string.match(newurl, "^%${")
+    ) then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local function set_new_params(newurl, data)
+    for param, value in pairs(data) do
+      if value == nil then
+        value = ""
+      elseif type(value) == "string" then
+        value = "=" .. value
+      end
+      if string.match(newurl, "[%?&]" .. param .. "[=&]") then
+        newurl = string.gsub(newurl, "([%?&]" .. param .. ")=?[^%?&;]*", "%1" .. value)
+      else
+        if string.match(newurl, "%?") then
+          newurl = newurl .. "&"
+        else
+          newurl = newurl .. "?"
+        end
+        newurl = newurl .. param .. value
+      end
+    end
+    return newurl
+  end
+
+  local function increment_param(newurl, param, default, step)
+    local value = string.match(newurl, "[%?&]" .. param .. "=([0-9]+)")
+    if value then
+      value = tonumber(value)
+      value = value + step
+      return set_new_params(newurl, {[param]=tostring(value)})
+    else
+      if default ~= nil then
+        default = tostring(default)
+      end
+      return set_new_params(newurl, {[param]=default})
+    end
+  end
+
+  local function get_count(data)
+    local count = 0
+    for _ in pairs(data) do
+      count = count + 1
+    end 
+    return count
+  end
+
+  if allowed(url)
+    and status_code < 300
+    and item_type ~= "asset" then
+    html = read_file(file)
+    if (string.match(url, "^https?://[^/]+/@?[0-9a-zA-Z_]+")
+      or string.match(url, "^https?://[^/]+/@?[^/]+/a/[0-9]+")
+      or string.match(url, "^https?://[^/]+/@?[^/]+/c/[0-9]+")
+      or string.match(url, "^https?://[^/]+/@?[^/]+/q/[0-9]+")
+    )
+      and not string.match(url, "[%?&]last_id=")
+      and not string.match(url, "/reactions$")
+      and not string.match(url, "/comments$") then
+      local path = string.match(url, "^https?://[^/]+/@?(.-)$")
+      check(urlparse.absolute(url, "/@" .. path))
+      check(urlparse.absolute(url, "/" .. path))
+    end
+    for d in string.gmatch(html, "(<turbo%-frame[^>]+>)") do
+      local src_url = string.match(d, 'src="([^"]+)"')
+      local id = string.match(d, 'id="([^"]+)"')
+      if src_url then
+        src_url = urlparse.absolute(url, src_url)
+        if id then
+          turbo_frame = id
+        end
+        check(src_url)
+        turbo_frame = nil
+        if string.match(src_url, "/comments")
+          or string.match(src_url, "/reactions") then
+          check(string.match(src_url, "^(.+)/[a-z]+$"))
+        end
+      end
+    end
+    for d in string.gmatch(html, '(<a [^>]*data%-turbo%-frame="modal"[^>]+>)') do
+      local href = string.match(d, 'href="([^"]+)"')
+      if href then
+        href = urlparse.absolute(url, href)
+        turbo_frame = "modal"
+        check(href)
+        turbo_frame = nil
+      end
+    end
+    if string.match(url, "^https?://[^/]+/@") then
+      for d in string.gmatch(html, "(<form .-</form>)") do
+        if string.match(d, "<button[^>]+>Load more</button>") then
+          local url_parameters = {}
+          for d2 in string.gmatch(d, "(<input [^>]+)") do
+            local name = string.match(d2, 'name="([^"]+)"')
+            local value = string.match(d2, 'value="([^"]+)"')
+            if not name or not value then
+              error("Not all URL parameter data found.")
+            end
+            if url_parameters[name] then
+              error("URL parameter found before.")
+            end
+            url_parameters[name] = value
+          end
+          local count = 0
+          local newurl = set_new_params(url, url_parameters)
+          check(newurl)
+        end
+      end
+    end
+    html = string.gsub(html, "<turbo%-frame[^>]+>", "")
+    html = string.gsub(html, "<form [^>]+>", "")
+    html = string.gsub(html, '<a [^>]*data%-turbo%-frame="modal"[^>]+>', "")
+    for newurl in string.gmatch(string.gsub(html, "&[qQ][uU][oO][tT];", '"'), '([^"]+)') do
+      if json then
+        check(newurl)
+      else
+        checknewurl(newurl)
+      end
+    end
+    for newurl in string.gmatch(string.gsub(html, "&#039;", "'"), "([^']+)") do
+      if json then
+        check(newurl)
+      else
+        checknewurl(newurl)
+      end
+    end
+    for newurl in string.gmatch(html, "[^%-]href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, '[^%-]href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, ":%s*url%(([^%)]+)%)") do
+      checknewurl(newurl)
+    end
+    html = string.gsub(html, "&gt;", ">")
+    html = string.gsub(html, "&lt;", "<")
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+      checknewurl(newurl)
+    end
+  end
+
+  return urls
+end
+
+wget.callbacks.write_to_warc = function(url, http_stat)
+  status_code = http_stat["statcode"]
+  set_item(url["url"])
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. " \n")
+  io.stdout:flush()
+  logged_response = true
+  if not item_name then
+    error("No item name found.")
+  end
+  is_initial_url = false
+  if http_stat["statcode"] ~= 200 then
+    retry_url = true
+    return false
+  end
+  if http_stat["len"] == 0
+    and http_stat["statcode"] < 300 then
+    retry_url = true
+    return false
+  end
+  if abortgrab then
+    print("Not writing to WARC.")
+    return false
+  end
+  retry_url = false
+  tries = 0
+  return true
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+
+  if not logged_response then
+    url_count = url_count + 1
+    io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. " \n")
+    io.stdout:flush()
+  end
+  logged_response = false
+
+  if killgrab then
+    return wget.actions.ABORT
+  end
+
+  set_item(url["url"])
+  if not item_name then
+    error("No item name found.")
+  end
+
+  if abortgrab then
+    abort_item()
+    return wget.actions.EXIT
+  end
+
+  if status_code == 0 or retry_url then
+    io.stdout:write("Server returned bad response. ")
+    io.stdout:flush()
+    tries = tries + 1
+    local maxtries = 11
+    if status_code == 401 or status_code == 403 then
+      os.execute("sleep 60")
+      tries = maxtries + 1
+    end
+    if tries > maxtries then
+      io.stdout:write(" Skipping.\n")
+      io.stdout:flush()
+      tries = 0
+      abort_item()
+      return wget.actions.EXIT
+    end
+    local sleep_time = math.random(
+      math.floor(math.pow(2, tries-0.5)),
+      math.floor(math.pow(2, tries))
+    )
+    io.stdout:write("Sleeping " .. sleep_time .. " seconds.\n")
+    io.stdout:flush()
+    os.execute("sleep " .. sleep_time)
+    return wget.actions.CONTINUE
+  else
+    if status_code == 200 then
+      if not seen_200[url["url"]] then
+        seen_200[url["url"]] = 0
+      end
+      seen_200[url["url"]] = seen_200[url["url"]] + 1
+    end
+    downloaded[url["url"]] = true
+  end
+
+  if status_code >= 300 and status_code <= 399 then
+    local newloc = urlparse.absolute(url["url"], http_stat["newloc"])
+    if processed(newloc) or not allowed(newloc, url["url"]) then
+      tries = 0
+      return wget.actions.EXIT
+    end
+  end
+
+  tries = 0
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local function submit_backfeed(items, key)
+    local tries = 0
+    local maxtries = 5
+    while tries < maxtries do
+      if killgrab then
+        return false
+      end
+      local body, code, headers, status = http.request(
+        "https://legacy-api.arpa.li/backfeed/legacy/" .. key,
+        items .. "\0"
+      )
+      if code == 200 and body ~= nil and cjson.decode(body)["status_code"] == 200 then
+        io.stdout:write(string.match(body, "^(.-)%s*$") .. "\n")
+        io.stdout:flush()
+        return nil
+      end
+      io.stdout:write("Failed to submit discovered URLs." .. tostring(code) .. tostring(body) .. "\n")
+      io.stdout:flush()
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+    end
+    kill_grab()
+    error()
+  end
+
+  local file = io.open(item_dir .. "/" .. warc_file_base .. "_bad-items.txt", "w")
+  for url, _ in pairs(bad_items) do
+    file:write(url .. "\n")
+  end
+  file:close()
+  for key, data in pairs({
+    ["retrospring-rwa6hbolipacccs3"] = discovered_items,
+    ["urls-mifbwshvuj9chv58"] = discovered_outlinks
+  }) do
+    print("queuing for", string.match(key, "^(.+)%-"))
+    local items = nil
+    local count = 0
+    for item, _ in pairs(data) do
+      print("found item", item)
+      if items == nil then
+        items = item
+      else
+        items = items .. "\0" .. item
+      end
+      count = count + 1
+      if count == 1000 then
+        submit_backfeed(items, key)
+        items = nil
+        count = 0
+      end
+    end
+    if items ~= nil then
+      submit_backfeed(items, key)
+    end
+  end
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if killgrab then
+    return wget.exits.IO_FAIL
+  end
+  if abortgrab then
+    abort_item()
+  end
+  return exit_status
+end
+
+
